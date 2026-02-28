6f33ddcc7315e4b3af55ffc6c35aceaeb3e552f8
Sobih <sobihmohamedsobih@gmail.com>
2026-02-28 15:06:49 +0200

    User & Address Entities Creation

---
+++ Core/E-commerce.Domain/Models/Address/AddressEntity.cs
@@ -0,0 +1,21 @@
+﻿using E_commerce.Domain.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce.Domain.Models.Address
+{
+    public class AddressEntity : BaseEntity<int>
+    {
+        public string City { get; set; } = string.Empty;
+        public string StreetDetails { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public bool IsDefault { get; set; } 
+
+        // Foreign Key
+        public Guid UserId { get; set; }
+
+        // Navigation Property: Each address belongs to one user
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
---
+++ Core/E-commerce.Domain/Models/User/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using E_commerce.Shared.EnumsHelper.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce.Domain.Models.User
+{
+    public class ApplicationUser : BaseEntity<Guid>
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public UserType Role { get; set; } = UserType.Customer;
+    }
+}
