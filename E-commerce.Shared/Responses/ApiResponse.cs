e77b3c49d25d45efc8b5c49d3c842881ff2f3594
Sobih <sobihmohamedsobih@gmail.com>
2026-03-10 02:03:19 +0200

    Build Api Response model

---
+++ E-commerce.Shared/Responses/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace E_commerce.Shared.Responses
+{
+    public class ApiResponse<TData>
+    {
+        public int StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public TData? Data { get; set; }
+        public List<string>? Errors { get; set; } = new List<string>();
+
+        public ApiResponse(TData data, string message, int statusCode = 200)
+        {
+            IsSuccess = true;
+            Message = message;
+            Data = data;
+            StatusCode = statusCode;
+        }
+        public ApiResponse( string message, List<string> errors,  int statusCode = 400)
+        {
+            IsSuccess = false;
+            Message = message;
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+
+    }
+}
