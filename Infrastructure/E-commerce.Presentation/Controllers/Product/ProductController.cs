0e4a9571dd4d6798c949d1a93700992da0a71969
Sobih <sobihmohamedsobih@gmail.com>
2026-04-07 21:11:33 +0200

    Create the prodcut controller

---
+++ Infrastructure/E-commerce.Presentation/Controllers/Product/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using E_commerce.Abstraction.IService.Product;
+using E_commerce.Shared.Common.Params.Product;
+using E_commerce.Shared.Dto_s.Product;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce.Presentation.Controllers.Product
+{
+    public class ProductController : AppBaseController
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // 1. GET ALL PRODUCTS WITH FILTERING, SORTING, PAGINATION
+        [HttpGet]
+        public async Task<ActionResult> GetAllProducts([FromQuery] ProductSpecParams specParams)
+        {
+            var products = await _productService.GetAllProductsAsync(specParams);
+            return Success(products, "Products get successfully");
+        }
+
+        // 2. get product details by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProductDetails(int id)
+        {
+            var product = await _productService.GetProductDetailsByIdAsync(id);
+            return Success(product, "product details get successfully");
+        }
+
+        // 3. create product
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult> CreateProduct([FromForm] ProductToCreateDto dto)
+        {
+            var product = await _productService.CreateProductAsync(dto);
+            return Created(product, "Prodcut Created Successfully");
+        }
+
+        // 4. update product
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(int id, [FromForm] ProductToUpdateDto dto)
+        {
+            var product = await _productService.UpdateProductAsync(id, dto);
+            return Success(product, "Product Updated Successfully");
+        }
+
+        // 5.delete product
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var result = await _productService.DeleteProductAsync(id);
+
+            if (!result)
+                return BadRequestError("Error While Deleting the product");
+
+            return Success("Product Deleted successfully");
+        }
+    }
+}
