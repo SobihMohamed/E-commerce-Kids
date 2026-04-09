6a8fe134b7d03579ba5186efbefc395fba0a5a06
Sobih <sobihmohamedsobih@gmail.com>
2026-04-09 15:14:09 +0200

    Add OrderController with user and admin order endpoints
    Introduced OrderController to handle order operations, including creating orders, retrieving user orders (summary and details), and updating order status (admin only). Endpoints are secured with authorization and use IOrderService for business logic. User ID is extracted from claims for user-specific actions.

---
+++ Infrastructure/E-commerce.Presentation/Controllers/Order/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using E_commerce.Abstraction.IService.Order;
+using E_commerce.Shared.Dto_s.Order;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace E_commerce.Presentation.Controllers.Order
+{
+    public class OrderController(IOrderService orderService) : AppBaseController
+    {
+        // 1.  create order
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> CreateOrderAsync([FromBody] OrderToCreateDto orderDto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var createdOrder = await orderService.CreateOrderAsync(userId!, orderDto);
+
+            return Created(createdOrder, "Order created successfully.");
+        }
+
+        // 2. (Summary)
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> GetOrdersForUserAsync()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var orders = await orderService.GetOrdersForUserAsync(userId!);
+
+            return Success(orders);
+        }
+
+        // 3.(Details)
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetOrderByIdForUserAsync(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var order = await orderService.GetOrderByIdForUserAsync(id, userId!);
+
+            return Success(order);
+        }
+
+        // 4. update order status (Admin only)
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}/status")]
+        public async Task<ActionResult> UpdateOrderStatusAsync(Guid id, [FromBody] UpdateOrderStatusDto statusDto)
+        {
+            var updatedOrder = await orderService.UpdateOrderStatusAsync(id, statusDto);
+            return Success(updatedOrder, "Order status has been updated successfully.");
+        }
+    }
+}
